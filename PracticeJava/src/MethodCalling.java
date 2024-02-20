ee8c102c696440f63653b3a3b75582e8870220eb
HP <HP@192.168.2.122>
2024-02-20 22:07:09 +0530

    20febb

--- PracticeJava/src/MethodCalling.java
+++ PracticeJava/src/MethodCalling.java
@@ -1,8 +1,30 @@
 
 public class MethodCalling {
+	
+	// Non-Static Methods ->
+	public void show1() {
+		System.out.println("Inside show 1");
+		show3();   // call method form another method
+	}
+	
+	public void show3() {
+		System.out.println("Inside show 3");
+		show2();
+	}
+
+
+	public void show2() {
+		System.out.println("Inside show 2");
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		MethodCalling obj= new MethodCalling ();
+		obj.show1();
+		obj.show2();
+		obj.show3();
 
 	}
 
