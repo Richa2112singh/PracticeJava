a152b2986bb50a14415442ab805f582b9ed238d7
HP <HP@192.168.2.122>
2024-02-20 21:38:13 +0530

    20feb

--- PracticeJava/src/Methods.java
+++ PracticeJava/src/Methods.java
@@ -4,6 +4,7 @@ public class Methods {
 	//Declaration -> Define
 	public int add(int num, int num2) {        //Addition
 		int sum = num + num2;
+		System.out.println("Addition is "+ sum);
 		
 		return sum; // Last statement of your method 
 		
@@ -11,7 +12,33 @@ public class Methods {
 	
 	public void test() {    // <-Method execution/call
 		System.out.println("Inside test Method");
+		
+		Methods obj = new Methods();
+		int  addition = obj.add(50, 10);
+		
+		Methods obj2 = new Methods();
+		int  subtraction = obj2.sub(50, 10);
+		
+		Methods obj3 = new Methods();	
+		String S4 = obj3.concat("Hi" , 'C');
 	}
+	
+	public int sub(int numb, int numb2) {           // Subtraction 
+		int subt = numb - numb2;
+		System.out.println("Subtraction is "+subt);
+		return subt; // Last statement of your method 
+	}
+	
+	public String concat(String S1 , char S2) {      // String 	
+		String S3 = S1+S2;	
+		System.out.println("String is" + S3);
+		
+//		Methods obj3 = new Methods();	
+//		String S4 = obj3.concat("Hi" , 'C');
+//		System.out.println(S4); 
+//		
+		return S3; // Last statement of your method 
+	}	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -19,34 +46,14 @@ public class Methods {
 		a=10;  // Initialization 
 		
 		Methods obj = new Methods();
-		obj.test();
-	//	int addition = obj.add(5, 10);
-	//	System.out.println(addition);
-		
-		System.out.println(obj.add(50, 10));
-
-	}
-
-	public int sub(int numb, int numb2) {           // Subtraction 
-		int subt = numb - numb2;
-		
 		Methods obj2 = new Methods();
-		int subtraction = obj2.sub(50, 10);
-		System.out.println(subtraction);
-		
-		return subt; // Last statement of your method 
-	}
+		obj.test();
+		obj.concat("trt", 'd');
 
-		
+
+	}
 	
-	public String concat(String S1 , char S2) {      // String 	
-		String S3 = S1+S2;	
-		
-		Methods obj3 = new Methods();	
-		String S4 = obj3.concat("Hi" , 'C');
-		System.out.println(S4); 
-		
-		return S3; // Last statement of your method 
-	}		
+	
+	
 		
 	}
--- PracticeJava/src/Typecast.java
+++ PracticeJava/src/Typecast.java
@@ -3,16 +3,29 @@ public class Typecast {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		double randomNum = Math.random()*10;
-		System.out.println(randomNum);
-		
-		// Predefined Method by java ->
+
+// Predefined Method by java ->
 //		Math.random();
-//	->	Returns a double value with a positive sign, greater than or equal to 0.0 and less than 1.0. 
+//	->	Returns a double value with a positive sign, greater than or equal to 0.0 and less than 1.0.
+//  ->  useful for OTPs
+			
+	double randomNum = Math.random()*10;
+	System.out.println(randomNum);
 		
 	double randomNum2 = Math.random()*100;
 	System.out.println(randomNum2);
+
+// Type Casting -> to get an integer value (decimal to integer)
+	
+	int randomNum3 = (int) (Math.random()*1000);
+	System.out.println(randomNum3);	
+	
+	// Convert any data type into Integer->
+	String RollNo = "21";
+	System.out.println(RollNo + 5);
+	int RollNoInt = Integer.parseInt(RollNo);
+	System.out.println(RollNoInt + 5);
+	
 		
 
 	}
