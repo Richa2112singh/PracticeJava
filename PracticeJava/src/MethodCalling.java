e074d0d058b51e66a3f97307b04058b9e342faac
HP <HP@192.168.2.122>
2024-02-21 06:35:51 +0530

    21feb

--- PracticeJava/src/MethodCalling.java
+++ PracticeJava/src/MethodCalling.java
@@ -1,20 +1,27 @@
 
 public class MethodCalling {
 	
-	// Non-Static Methods ->
+	// Non-Static Methods -> // Non-static Method can call Static Method 
 	public void show1() {
 		System.out.println("Inside show 1");
-		show3();   // call method form another method
+		show2();   // call method form another method
 	}
 	
-	public void show3() {
-		System.out.println("Inside show 3");
-		show2();
+	public void show2() {
+		System.out.println("Inside show 2");
+		show3();
 	}
 
 
-	public void show2() {
-		System.out.println("Inside show 2");
+	public void show3() {
+		System.out.println("Inside show 3");
+		//statMethod ();
+	}
+	
+	// Static Method can not call Non-static Method 
+	public static void statMethod () {
+		System.out.println("Inside Stat Method");
+	//	show3();
 	}
 
 
@@ -23,8 +30,11 @@ public class MethodCalling {
 		
 		MethodCalling obj= new MethodCalling ();
 		obj.show1();
+		statMethod ();
 		obj.show2();
 		obj.show3();
+		
+		
 
 	}
 
