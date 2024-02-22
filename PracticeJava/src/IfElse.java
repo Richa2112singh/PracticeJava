ca46d972178f4c507c1821fecd75ac9c275cb994
HP <HP@192.168.2.122>
2024-02-22 22:31:38 +0530

    ifelse

--- PracticeJava/src/IfElse.java
+++ PracticeJava/src/IfElse.java
@@ -3,7 +3,21 @@ public class IfElse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int num = (int) (Math.random()*10);
+		System.out.println(num);
+		
+		if (num>5) {
+			System.out.println("Greater than 5"); 
+		    System.out.println("Still Greater than 5");
+	    }
+		
+		else if (num<3) {
+			System.out.println("lesser than 3");
+		}
+		else {
+			System.out.println("Less than 5");
+        }
 	}
 
 }
---
+++ PracticeJava/src/switchCase.java
@@ -0,0 +1,38 @@
+
+public class switchCase {
+	
+	int day = 2;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int day = 6;
+		
+		switch (day) {
+		case 1 :
+			System.out.println("Hey its Monday");
+		    break;
+			
+		case 2 :
+			System.out.println("Hey its Tuesday");
+			break;
+			
+		case 3 :
+			System.out.println("Hey its Wednesday");
+			break;
+		
+		case 4 :
+			System.out.println("Hey its Thursday");
+			break;
+		
+		case 5 :
+			System.out.println("Hey its Friday");
+			break;
+			
+		default :
+			System.out.println("No such weekday");
+		}
+
+	}
+
+}
